7152354f2fe20ff434952367b0d92bad8be1ecda
Joseph Brant <josephrbrant@gmail.com>
2015-04-14 01:07:57 -0400

    Final changes to Python Maze Generator Mockup
    Changes include:
    - Added logic for T hallways
    - Added logic for showing the Starting Lobby
    - Added logic for picking and showing Ending lobby(ies)
    - Cleaned the code
    
    This should complete our Python Mockup. All that remains is to port this
    to Unity and test numbers in a 3D environment - they should be very easy
    to change if the port remains true to the Python version.

--- MazeTest/MazeTest.py
+++ MazeTest/MazeTest.py
@@ -4,6 +4,32 @@ import random
 import pygame
 import os
 
+###########
+# DEFINES #
+###########
+
+# Min and max percent densities of the maze. Based on tests, can be adjusted later very easily.
+minDensity = 0.70
+maxDensity = 1.00
+
+# Width and height of the overall grid the maze CAN occupy.
+width = 15
+height = 15
+
+# Starting position of the first lobby.
+startX = 8
+startY = 8
+
+# Minimum distance the end point can be from the starting point.
+minDistance = 6
+
+# The number of possible end points. This is if we decide one end point is too cruel.
+numEndPoints = 1
+
+###############
+# END DEFINES #
+###############
+
 pygame.init()
 
 class Tile(IntEnum):
@@ -15,6 +41,12 @@ class Tile(IntEnum):
 	CSW = 5
 	HNS = 6 #hall running from north to south
 	HEW = 7
+	TWSE = 8	# T shape running West, South, and East
+	TWNE = 9
+	TNES = 10
+	TNWS = 11
+	START = 12
+	END = 13
 
 class Dir(Enum):
 	N = 0
@@ -24,24 +56,20 @@ class Dir(Enum):
 
 class MazeBuilder:
 
-	def __init__(self, width, height):
-		self.width = width
-		self.height = height
+	def __init__(self):
 		self.maze = [[Tile.WALL for i in range(width)] for i in range(height)]
 		self.tileImgs = []
 		self.screen = None
 		self.numTiles = 0
 
 		area = width * height
-		minDensity = .25 #based on tests to find best maze
-		maxDensity = .60
 		self.minTiles = minDensity * area
 		self.maxTiles = maxDensity * area
 		self.lobbies = []
 
 
 	def isLegal(self, x, y):
-		if x >= self.width or y >= self.height:
+		if x >= width or y >= height:
 			return False
 
 		if x < 0 or y < 0:
@@ -72,27 +100,35 @@ class MazeBuilder:
 		return cn, rn
 
 	def getTiles(self, ndir):
-		tiles = [Tile.LOBBY, Tile.CNE, Tile.CNW, Tile.CSE, Tile.CSW, Tile.HNS, Tile.HEW]
+		tiles = [Tile.LOBBY, Tile.CNE, Tile.CNW, Tile.CSE, Tile.CSW, Tile.HNS, Tile.HEW, Tile.TWSE, Tile.TWNE, Tile.TNES, Tile.TNWS]
 
 		if ndir == Dir.N:
 			tiles.remove(Tile.CNE)
 			tiles.remove(Tile.CNW)
 			tiles.remove(Tile.HEW)
+			tiles.remove(Tile.TWNE)
 		elif ndir == Dir.S:
 			tiles.remove(Tile.CSE)
 			tiles.remove(Tile.CSW)
 			tiles.remove(Tile.HEW)
+			tiles.remove(Tile.TWSE)
 		elif ndir == Dir.E:
 			tiles.remove(Tile.CNE)
 			tiles.remove(Tile.CSE)
 			tiles.remove(Tile.HNS)
+			tiles.remove(Tile.TNES)
 		elif ndir == Dir.W:
 			tiles.remove(Tile.CNW)
 			tiles.remove(Tile.CSW)
 			tiles.remove(Tile.HNS)
+			tiles.remove(Tile.TNWS)
 
 		return tiles
-
+		
+	def curDistance(self, curX, curY):
+		x = abs(startX - curX)
+		y = abs(startY - curY)
+		return x + y
 
 	def buildMaze(self, x, y, tile, ldir):
 
@@ -100,6 +136,7 @@ class MazeBuilder:
 		r = y
 		cn = -1
 		rn = -1
+		ndir = None
 
 		self.maze[c][r] = tile
 		self.numTiles += 1
@@ -107,9 +144,9 @@ class MazeBuilder:
 		# if self.stepDisplay(c, r, tile):
 		# 	return True
 
-		#print(tile)
-
-		if tile == Tile.LOBBY:
+		if tile == Tile.LOBBY or tile == Tile.START:
+			if self.curDistance(x, y) >= minDistance:
+				self.lobbies.append((x, y))
 			
 			dirs = [Dir.N, Dir.S, Dir.E, Dir.W]
 			random.shuffle(dirs)
@@ -121,10 +158,76 @@ class MazeBuilder:
 					self.buildMaze(cn, rn, random.choice(tiles), d)
 
 			return
-
-		ndir = None
-
-		if tile == Tile.CNE:
+			
+		if tile == Tile.TWSE:
+			dirs = [Dir.W, Dir.S, Dir.E]
+			random.shuffle(dirs)
+			
+			if ldir == Dir.E:
+				dirs.remove(Dir.W)
+			elif ldir == Dir.N:
+				dirs.remove(Dir.S)
+			elif ldir == Dir.W:
+				dirs.remove(Dir.E)
+				
+			for d in dirs:
+				cn, rn = self.getNext(c, r, d)
+				if self.isLegal(cn, rn):
+					tiles = self.getTiles(d)
+					self.buildMaze(cn, rn, random.choice(tiles), d)
+		
+		elif tile == Tile.TWNE:
+			dirs = [Dir.W, Dir.N, Dir.E]
+			random.shuffle(dirs)
+			
+			if ldir == Dir.E:
+				dirs.remove(Dir.W)
+			elif ldir == Dir.S:
+				dirs.remove(Dir.N)
+			elif ldir == Dir.W:
+				dirs.remove(Dir.E)
+				
+			for d in dirs:
+				cn, rn = self.getNext(c, r, d)
+				if self.isLegal(cn, rn):
+					tiles = self.getTiles(d)
+					self.buildMaze(cn, rn, random.choice(tiles), d)
+					
+		elif tile == Tile.TNES:
+			dirs = [Dir.N, Dir.E, Dir.S]
+			random.shuffle(dirs)
+			
+			if ldir == Dir.S:
+				dirs.remove(Dir.N)
+			elif ldir == Dir.W:
+				dirs.remove(Dir.E)
+			elif ldir == Dir.N:
+				dirs.remove(Dir.S)
+				
+			for d in dirs:
+				cn, rn = self.getNext(c, r, d)
+				if self.isLegal(cn, rn):
+					tiles = self.getTiles(d)
+					self.buildMaze(cn, rn, random.choice(tiles), d)
+					
+		elif tile == Tile.TNWS:
+			dirs = [Dir.N, Dir.W, Dir.S]
+			random.shuffle(dirs)
+			
+			if ldir == Dir.S:
+				dirs.remove(Dir.N)
+			elif ldir == Dir.E:
+				dirs.remove(Dir.W)
+			elif ldir == Dir.N:
+				dirs.remove(Dir.S)
+				
+			for d in dirs:
+				cn, rn = self.getNext(c, r, d)
+				if self.isLegal(cn, rn):
+					tiles = self.getTiles(d)
+					self.buildMaze(cn, rn, random.choice(tiles), d)
+		
+		elif tile == Tile.CNE:
 			if ldir == Dir.S:
 				ndir = Dir.E
 			elif ldir == Dir.W:
@@ -168,8 +271,7 @@ class MazeBuilder:
 			for f in files:
 				self.tileImgs.append(pygame.image.load(os.path.join(imgDir, f)))
 
-
-		self.screen = pygame.display.set_mode((400,400))
+		self.screen = pygame.display.set_mode((width * 20, height * 20))
 
 	def stepDisplay(self, x, y, tile):
 		xc = x * 20
@@ -183,28 +285,38 @@ class MazeBuilder:
 				pygame.display.quit()
 				return True
 
-
 		self.screen.blit(self.tileImgs[int(tile)], (xc, yc))
 		pygame.display.flip()
 
 
 	def checkMaze(self):
+		# Checks the number of tiles in the maze. If inappropriate, or if there is no eligible lobby for an end point, rebuild the maze.
 		print(self.numTiles)
 
-		if self.numTiles < self.minTiles or self.numTiles > self.maxTiles:
+		if self.numTiles < self.minTiles or self.numTiles > self.maxTiles or len(self.lobbies) < numEndPoints:
+			self.lobbies = []
 			self.numTiles = 0
-			self.maze = [[Tile.WALL for i in range(self.width)] for i in range(self.height)]
-			self.buildMaze(10, 10, Tile.LOBBY, Dir.W)
+			self.maze = [[Tile.WALL for i in range(width)] for i in range(height)]
+			self.buildMaze(startX, startY, Tile.START, Dir.W)
 			return False
 
 		return True
+		
+	def pickEndPoints(self):
+		# Assume self.lobbies is NOT empty; this method is called AFTER checkMaze, which assures this
+		random.shuffle(self.lobbies)
+		for i in range(len(self.lobbies)):
+			c = self.lobbies[i][0]
+			r = self.lobbies[i][1]
+			self.maze[c][r] = Tile.END
+			if i >= numEndPoints - 1:
+				break
 
 	def displayMaze(self):
-
 		x = 0
 		y = 0
 
-		for i in maze.maze:
+		for i in self.maze:
 			for j in i:
 				self.screen.blit(self.tileImgs[int(j)], (x, y))
 				y += 20
@@ -214,20 +326,22 @@ class MazeBuilder:
 			y = 0
 			
 		pygame.display.flip()
+###################################################################
+# END CLASS DECLARATION
 
-w = 20
-h = 20
-
-maze = MazeBuilder(w,h)
+maze = MazeBuilder()
 
 maze.initDisplay()
 
-maze.buildMaze(10, 10, Tile.LOBBY, Dir.W)
+maze.buildMaze(startX, startY, Tile.START, Dir.W)
 
 while not maze.checkMaze():
 	continue
+	
+maze.pickEndPoints()
 
 maze.displayMaze()
+print ("Lobbies: " + str(len(maze.lobbies)))
 
 done = False
 while(not done):
@@ -235,5 +349,3 @@ while(not done):
 	if evt.type == pygame.QUIT:
 		pygame.display.quit()
 		done = True
-
-#x = 1.436, y = 1.375, z = .934
