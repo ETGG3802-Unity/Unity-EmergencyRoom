fdd357d5ece719159730abb4ddd334369d9a9ce8
Don Loughry <dloughry32@gmail.com>
2015-04-14 15:26:44 -0400

    Updated First Person Camera Script

--- Assets/Scripts/Main_Input.cs
+++ Assets/Scripts/Main_Input.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Main_Input : MonoBehaviour {
 
-	public KeyCode forward, left, right, backward;
+	public KeyCode forward, backward;
 	public Vector3 pz;
 	public float maxVelocity;
 	public GameObject head;
@@ -11,38 +11,39 @@ public class Main_Input : MonoBehaviour {
 
 	private float bufferSpacePos, bufferSpaceNeg, deltaRotation, maxPitch;
 	private Rigidbody charRigidBody;
-	private Vector3 maxVelVector;
 	private bool moving;
 
 	// Use this for initialization
 	void Start () {
 		charRigidBody = gameObject.GetComponent<Rigidbody> ();
-		maxVelVector = new Vector3 (charRigidBody.velocity.x, charRigidBody.velocity.y, maxVelocity);
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
 	}
 
 	void FixedUpdate(){
 		moving = false;
+
+		// Cap player speed! I hate making new Vector3's during the physics loop, though...//
 		if (charRigidBody.velocity.z >= maxVelocity)
-			charRigidBody.velocity = maxVelVector;
+			charRigidBody.velocity = new Vector3(charRigidBody.velocity.x, charRigidBody.velocity.y, maxVelocity);
+		else if (charRigidBody.velocity.z <= -maxVelocity) {
+			charRigidBody.velocity = new Vector3(charRigidBody.velocity.x, charRigidBody.velocity.y, -maxVelocity);
+		}
+		///////////////////////
+		/// 
+		/// Movement Code! //
 		if (Input.GetKey(forward)) {
-			charRigidBody.AddForce(transform.forward * 0.3f, ForceMode.VelocityChange);
+			charRigidBody.AddForce(transform.forward * 0.6f, ForceMode.VelocityChange);
 			moving = true;
 		}
 		if (Input.GetKey (backward)) {
-			charRigidBody.AddForce(-transform.forward * 0.3f, ForceMode.VelocityChange);
-			moving = true;
-		}
-		if (Input.GetKey (left)) {
-			charRigidBody.AddForce(-transform.right * 0.3f, ForceMode.VelocityChange);
-			moving = true;
-		}
-		if (Input.GetKey (right)) {
-			charRigidBody.AddForce(transform.right * 0.3f, ForceMode.VelocityChange);
+			charRigidBody.AddForce(-transform.forward * 0.6f, ForceMode.VelocityChange);
 			moving = true;
 		}
 		if (!moving) {
 			charRigidBody.velocity = Vector3.zero; //stop all movement.
 		}
+		/////////////////////////
 	}
 	
 	// Update is called once per frame
@@ -52,38 +53,29 @@ public class Main_Input : MonoBehaviour {
 	 * we need to take the x-component of our mouse's movement and map that to the x-rotation of our camera, and the same 
 	 * deal for "y".
 	*/
-		// Stupid simple version:
-		pz = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-		bufferSpacePos = 0.6f; // the viewport point at which rotation increased along an axis.
-		bufferSpaceNeg = 0.4f; // the viewport point at which rotation increased along an axis.
-		deltaRotation = 1.7f; // how quickly we rotate around an axis
-		maxPitch = 60.0f; // to cap vertical rotation, to be implemented later!
+		// Broken feature in Unity 5.1! We have to force-update the state of the cursor... and whether or not
+		// it works completely depends on outside influences (state of machine, data corruption, etc.)
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		//////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		// "0" Position on ViewPort Space is (0.5, 0.5)
+		maxPitch = 60.0f; // to cap vertical rotation, to be implemented later!
 		/**
 		 * Current issues:
-		 * * This isn't how first-person camera's work!!!
+		 * * WTF is with the unyeilding momentum?
 		 * 
 		 * Sample rotation code from: http://answers.unity3d.com/questions/288948/rotate-around-local-y-axis.html
 		 * transform.RotateAround(transform.position, transform.up, Time.deltaTime * 90f);
 		 **/
 		// Vertical Rotation Rotates the CAMERA //
-		if (pz.y > bufferSpacePos) {
-			head.transform.RotateAround (head.transform.position, head.transform.right, (pz.y - 0.5f) * -deltaRotation);
+		if (Input.GetAxis("Mouse Y") > 0 || Input.GetAxis ("Mouse Y") < 0) {
+			head.transform.RotateAround (head.transform.position, head.transform.right, -Input.GetAxis ("Mouse Y"));
 		}
-		if (pz.y < bufferSpaceNeg) {
-			head.transform.RotateAround (head.transform.position, head.transform.right, (0.5f - pz.y) * deltaRotation);
-		} 
-
 		// Horizontal Rotation Rotates the PLAYER //
-		if (pz.x > bufferSpacePos) {
-			transform.RotateAround (transform.position, Vector3.up, (pz.x - 0.5f) * deltaRotation);
+		if (Input.GetAxis("Mouse X") > 0 || Input.GetAxis ("Mouse X") < 0) {
+			transform.RotateAround (transform.position, Vector3.up, Input.GetAxis("Mouse X"));
 			//head.transform.RotateAround (head.transform.position, head.transform.up, (pz.x - 0.5f) * deltaRotation);
 		}
-		if (pz.x < bufferSpaceNeg) {
-			transform.RotateAround (transform.position, Vector3.up, (0.5f - pz.x) * -deltaRotation);
-			//head.transform.RotateAround (head.transform.position, head.transform.up, (0.5f - pz.x) * -deltaRotation);
-		} 
 	}
 
 }
