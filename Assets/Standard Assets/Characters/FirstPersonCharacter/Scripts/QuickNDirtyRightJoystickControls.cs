3e6a05646783cdd5f1c29ad2db432fe6b0285954
funnyhomeboy <edwardst@mymail.shawnee.edu>
2015-04-22 20:49:05 -0400

    This has the First Person Camera. Works as a Prefab you can pop in.

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/QuickNDirtyRightJoystickControls.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuickNDirtyRightJoystickControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float angH = Input.GetAxis ("RightH");
+		float angV = Input.GetAxis ("RightV");
+		transform.localEulerAngles = new Vector3(angV, angH, 0);
+	}
+}
