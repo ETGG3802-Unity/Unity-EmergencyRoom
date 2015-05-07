b947abf2b804b8212eedc2ac68c5a3dd003d661d
Don Loughry <dloughry32@gmail.com>
2015-05-07 00:37:23 -0400

    Raycast grab and ragdoll.

--- Assets/Scripts/RayCast_Grab.cs
+++ Assets/Scripts/RayCast_Grab.cs
@@ -6,35 +6,72 @@ using UnityStandardAssets.CrossPlatformInput;
 public class RayCast_Grab : MonoBehaviour {
 
 	public GameObject head;
+	public float mGrabForwardOffset, mGrabUpOffset;
 	[SerializeField] float mRotationSensitivity;
 	
 	private FirstPersonController fpControl;
+	private SimpleCarController carControl;
+	private Camera fpCam;
+	private Camera carCam;
 	private RaycastHit hit;
 	private float sightDistance = 10.0f;
+	private bool hasObject = false;
 
 	void Start () {
-		// reference this game object to access FirstPersonController script
-		// from Unity standard assets.
-		fpControl = gameObject.GetComponent<FirstPersonController> ();
+		// References to the First Person Controller script and main camera. //
+		//fpControl = gameObject.GetComponent<FirstPersonController> ();
+		//fpCam = GameObject.Find ("FirstPersonCharacter").GetComponent<Camera>();
+		///////////////////////////////////////////////////////////////////////
+		// References to the Car Controller and main camera. //
+		//carControl = GameObject.Find ("Car_root").GetComponent <SimpleCarController>();
+		// Note that the below may not work... //
+		//carCam = GameObject.Find ("Car_root").GetComponent("Main Camera");
+		// You control the character by default... // 
+		/*carControl.enabled = false; // check later for stupidity...
+		carCam.enabled = false;
+		fpControl.enabled = true;
+		fpCam.enabled = true;*/
+		/////////////////////////////////////////////
+		// Eventually, we need to get a specific reference to the RAGDOLL, and set his parts //
+		// to NOT be kinematic whenever we release the mouse button. //
 	}
 	
 
 	void Update () 
 	{
-		if (Input.GetMouseButton (0))
-		{
-			if(Physics.Raycast(head.transform.position, head.transform.forward, out hit, sightDistance))
-			{
-				float horizontal = CrossPlatformInputManager.GetAxis("Mouse X");
-				float vertical = CrossPlatformInputManager.GetAxis("Mouse Y");
+		Quaternion moveRotation = head.transform.rotation;
+		float horizontal = CrossPlatformInputManager.GetAxis ("Mouse X");
+		float vertical = CrossPlatformInputManager.GetAxis ("Mouse Y");
+		GameObject.Find ("TargetPosition").transform.RotateAround (head.transform.position, head.transform.right, vertical);
+		if (Input.GetMouseButton (0)) {
+			if (Physics.Raycast (head.transform.position, head.transform.forward, out hit, sightDistance)) {
 				print (horizontal);
 				GameObject other = hit.transform.gameObject;
-				Rigidbody otherRBody = other.GetComponent<Rigidbody>();
-				if(other.tag == "Grabbable"){
-					other.transform.RotateAround(head.transform.position, head.transform.up, horizontal);
-					other.transform.RotateAround(head.transform.position, head.transform.right, -vertical);
+				if (other.tag == "Grabbable") {
+					// PHYSICS GRABBING STUFF //
+					hasObject = true;
+					if (other.GetComponent<Rigidbody> () != null) {
+						other.GetComponent<Rigidbody> ().isKinematic = true;
+						other.transform.parent = gameObject.transform;
+						other.transform.position = Vector3.MoveTowards(other.transform.position, GameObject.Find ("TargetPosition").transform.position, 1.0f);
+						other.transform.rotation = moveRotation;
+					} else {
+						print ("No RIGiDBOdY LOL BitCH");
+					}
+				} else if (other.tag == "CarController" && !hasObject) {
+					// SWITCH CONTROL TO CAR //
+					/*fpControl.enabled = false;
+					fpCam.enabled = false;
+					carControl.enabled = true;
+					carCam.enabled = true;*/
+				} else {
+					print ("Watch your trigger finger, nothing to grab here!");
 				}
 			}
+		} else {
+			hasObject = false;
+			GameObject.Find ("Cube").transform.parent = null;
+			GameObject.Find ("Cube").GetComponent<Rigidbody>().isKinematic = false;
 		}
 	}
 }
